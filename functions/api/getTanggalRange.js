f07fe79d91edb5c7dfe82802fb05bf7a24682c74
muqoddammtb <muqoddammtb@gmail.com>
2025-10-04 22:41:34 +0700

    Batch replace: "dickypagesdev" -> "muqoddammtb" | getTanggalRange.js | hits=1

--- functions/api/getTanggalRange.js
+++ functions/api/getTanggalRange.js
@@ -8,7 +8,7 @@ const CORS = {
   "Access-Control-Allow-Headers": "Content-Type, Authorization",
 };
 
-const OWNER = "dickypagesdev";
+const OWNER = "muqoddammtb";
 const REPO  = "server";
 const BRANCH = "main";
 
